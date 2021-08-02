f51992e2fe4249e8cb545c69d646b73a807a4c52
pacificsuraj <stsurajst@gmail.com>
2021-08-02 21:27:15 +0545

    route added

--- src/app.ts
+++ src/app.ts
@@ -10,14 +10,21 @@ class App {
     constructor(appInt: { port: number, middleware: any, controller: any }) {
         this.app = express()
         this.port = appInt.port
-
+        this.routes(appInt.controller) 
     } 
 
     public listen(){
         this.app.listen(this.port, () => {
             console.log(`Aap has started on port ${this.port}`)
         })
-    }   
+    }  
+    private routes(controllers){
+        controllers.forEach(controller => {
+            console.log(controller.path)
+            this.app.use(controller.path, controller.router)
+        });
+
+    } 
 }
 
 export default App
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import App from "./app";
 
+import HomeController from './controller/home.controller'
+
 const app = new App({
     port: 3000,
-    controller:[],
+    controller:[
+        new HomeController()
+    ],
     middleware:[]
 })
 
